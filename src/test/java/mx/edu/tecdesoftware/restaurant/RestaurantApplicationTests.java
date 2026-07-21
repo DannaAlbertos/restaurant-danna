da850d516bad23eb6b28aa5e236dc4331b8e1b5d
DannaAlbertos <danna.albertos@tecdesoftware.edu.mx>
2026-07-21 07:21:12 -0600

    First commit

---
+++ src/test/java/mx/edu/tecdesoftware/restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.tecdesoftware.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
